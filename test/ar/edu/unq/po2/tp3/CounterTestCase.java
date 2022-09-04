a699f21e0f6ac222c222f155891444b13067c5e6
Florencia06P <fpalavecino743@gmail.com>
2022-09-04 00:09:57 -0300

    version inicial

--- test/ar/edu/unq/po2/tp3/CounterTestCase.java
+++ test/ar/edu/unq/po2/tp3/CounterTestCase.java
@@ -6,6 +6,41 @@ import org.junit.jupiter.api.Test;
 
 class CounterTestCase {
 
+	private Counter counter;
+	
+	/**
+	* Crea un escenario de test básico, que consiste en un contador
+	* con 10 enteros
+	*
+	* @throws Exception
+	*/
+	@BeforeEach
+	public void setUp() throws Exception {
+	//Se crea el contador
+	counter = new Counter();
+	//Se agregan los numeros. Un solo par y nueve impares
+	counter.addNumber(1);
+	counter.addNumber(3);
+	counter.addNumber(5);
+	counter.addNumber(7);
+	counter.addNumber(9);
+	counter.addNumber(1);
+	counter.addNumber(1);
+	counter.addNumber(1);
+	counter.addNumber(1);
+	counter.addNumber(4);
+	}
+	/**
+	* Verifica la cantidad de pares
+	*/
+	@Test
+	public void testEvenNumbers() {
+	// Getting the even occurrences
+	int amount = counter.getEvenOcurrences();
+	// I check the amount is the expected one
+	assertEquals(amount, 9);
+	}
+	}
 	@Test
 	void test() {
 		fail("Not yet implemented");
