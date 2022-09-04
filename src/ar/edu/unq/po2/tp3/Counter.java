8b75884ae2e5835e65d9f1afc86f2d08b2a16067
Florencia06P <fpalavecino743@gmail.com>
2022-09-03 23:26:14 -0300

    version inicial

---
+++ src/ar/edu/unq/po2/tp3/Counter.java
@@ -0,0 +1,18 @@
+package ar.edu.unq.po2.tp3;
+
+public class Counter {
+	private int[] numeros;
+
+	public Counter(int[] numeros) {
+		super();
+		this.numeros = numeros;
+	}
+
+	public int[] getNumeros() {
+		return numeros;
+	}
+
+	public void setNumeros(int[] numeros) {
+		this.numeros = numeros;
+	}
+}
